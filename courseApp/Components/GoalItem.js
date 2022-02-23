a256633134ac59cd19ffc07dbd06521635fe8ffe
RauliYoung <tristan.ellenberg@gmail.com>
2022-02-23 13:52:53 +0200

    more styles

--- courseApp/Components/GoalItem.js
+++ courseApp/Components/GoalItem.js
@@ -5,7 +5,7 @@ const GoalItem = (props) => {
   return (
     <TouchableOpacity onPress={props.onDelete.bind(this, props.id)}>
       <View style={styles.listItem}>
-        <Text>{props.title}</Text>
+        <Text style={styles.textStyle}>{props.title}</Text>
       </View>
     </TouchableOpacity>
   );
@@ -15,9 +15,13 @@ const styles = StyleSheet.create({
   listItem: {
     padding: 10,
     marginVertical: 10,
-    backgroundColor: "#ccc",
-    borderColor: "black",
+    backgroundColor: "#00000000",
+    borderColor: "white",
     borderWidth: 1,
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
   },
 });
 export default GoalItem;
