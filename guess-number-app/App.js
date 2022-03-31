40950acf0869b26b646b5efbe895a67eaec0c6cb
RauliYoung <tristan.ellenberg@gmail.com>
2022-03-31 18:06:53 +0300

    new features

--- guess-number-app/App.js
+++ guess-number-app/App.js
@@ -1,13 +1,27 @@
-import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+
 import Header from "./components/Header";
 import StartGameScreen from "./screens/StartGameScreen";
+import GameScreen from "./screens/GameScreen";
 
 const App = () => {
+  const [userNumber, setUserNumber] = useState();
+
+  const startGameHandler = (selctedNumber) => {
+    setUserNumber(selctedNumber);
+  };
+
+  let content = <StartGameScreen onStartGame={startGameHandler} />;
+
+  if (userNumber) {
+    content = <GameScreen userChoice={userNumber} />;
+  }
+
   return (
     <View style={styles.screen}>
       <Header title="Guess a number" />
-      <StartGameScreen />
+      {content}
     </View>
   );
 };
--- guess-number-app/screens/GameScreen.js
+++ guess-number-app/screens/GameScreen.js
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
+import Card from "../components/Card";
+import NumberContainer from "../components/NumberContainer";
 
 const generateRandomBetween = (min, max, exclude) => {
   min = Math.ceil(min);
   max = Math.floor(max);
-  const randomNumber = Math.random() * (max - min) + min;
+  const randomNumber = Math.floor(Math.random() * (max - min)) + min;
   if (randomNumber === exclude) {
     return generateRandomBetween(min, max, exclude);
+  } else {
+    return randomNumber;
   }
 };
 
@@ -14,8 +18,31 @@ const GameScreen = (props) => {
   const [currentGuess, setCurrentGuess] = useState(
     generateRandomBetween(1, 100, props.userChoice)
   );
+  return (
+    <View style={styles.screen}>
+      <Text>Opponetn's Guess</Text>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <Card style={styles.buttonContainer}>
+        <Button title="Lower" onPress={() => {}} />
+        <Button title="Greater" onPress={() => {}} />
+      </Card>
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 20,
+    width: 300,
+    maxWidth: "80%",
+  },
+});
 
 export default GameScreen;
